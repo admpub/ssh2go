f7ecebea5d63a6757471c865c0285356a1efc6b4
Hank Shen <swhbox@foxmail.com>
2018-09-23 00:06:27 +0800

    update

---
+++ examples/ssh2go-simple-sshd/generate_cert.sh
@@ -0,0 +1,2 @@
+ssh-keygen -t rsa -P '' -f ./cert/id_rsa
+ssh-keygen -t dsa -P '' -f ./cert/id_dsa
---
+++ examples/ssh2go-simple-sshd/run.sh
@@ -0,0 +1,2 @@
+go install github.com/admpub/ssh2go/examples/ssh2go-simple-sshd
+ssh2go-simple-sshd --dsakey=./cert/id_dsa --rsakey=./cert/id_rsa --port 8888 -V
