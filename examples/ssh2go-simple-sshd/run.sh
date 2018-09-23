dc90332e343c23a0bc279fa0b0cc8e0e12b770a1
Hank Shen <swhbox@foxmail.com>
2018-09-23 11:42:27 +0800

    update

--- examples/ssh2go-simple-sshd/run.sh
+++ examples/ssh2go-simple-sshd/run.sh
@@ -1,2 +1,3 @@
 go install github.com/admpub/ssh2go/examples/ssh2go-simple-sshd
-ssh2go-simple-sshd --dsakey=./cert/id_dsa --rsakey=./cert/id_rsa --port 8888 -V
+# ssh2go-simple-sshd --dsakey=./cert/id_dsa --rsakey=./cert/id_rsa --port 8888 -V
+ssh2go-simple-sshd --dsakey=./cert/id_dsa --rsakey=./cert/id_rsa --port 8888
